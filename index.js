a2acacb873e53791d4d5977364f89f46c705adca
Kimbangg <ehehdgus1@hanyang.ac.kr>
2021-01-06 13:05:48 +0900

    install nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express'); // express 모듈을 가져온다.
 const app = express(); // express module을 만든다
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//apllication/x-www-form-urlencoded => 분석해서 가져오는 것
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json => 분석해서 가져오는 것
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 mongoose
   .connect('mongodb+srv://Bang:ehd123@boilerplate.yr5lv.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -13,7 +21,20 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('이번 주까지 마무리!');
+});
+
+app.post('/register', (req, res) => {
+  // 회원기입 시, 필요한 정보를 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
